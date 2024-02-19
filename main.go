2f25079fc124836536812da5fb34e648e0652ff2
MarkTBSS <marktbss@gmail.com>
2024-02-19 11:25:54 +0700

    [MOD] Timestamp

--- main.go
+++ main.go
@@ -3,45 +3,53 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-func SayB(wg *sync.WaitGroup) {
-	fmt.Println("Hello, world : B")
+func SayA(wg *sync.WaitGroup) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("A")
 	wg.Done()
 }
 
 func main() {
+	startSyncTime := time.Now()
 	// Synchronous or Sequential
-	fmt.Println("Hello, world : 1")
-	fmt.Println("Hello, world : 2")
-	fmt.Println("Hello, world : 3")
-	fmt.Println("Hello, world : 4")
-	fmt.Println("Hello, world : 5")
+	func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("1")
+	}()
+	func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("2")
+	}()
+	func() {
+		time.Sleep(1 * time.Second)
+		fmt.Println("3")
+	}()
+	fmt.Println("Total Run Time : ", time.Since(startSyncTime))
 	fmt.Println("================")
+	time.Sleep(2 * time.Second)
+	startAsyncTime := time.Now()
 	// Asynchronous
 	var wg sync.WaitGroup // สร้างตัวแปร WaitGroup
-	wg.Add(4)             // เพิ่มจำนวน goroutine ที่ต้องรอใน WaitGroup
+	wg.Add(3)             // เพิ่มจำนวน goroutine ที่ต้องรอใน WaitGroup
 	// ===== แบบนี้ไม่ได้เพราะอยู่นอก Scope =====
 	// go fmt.Println("Hello, world : A")
 	// wg.Done()
 	// ====================================
+	go SayA(&wg) // ส่งทางการอ้างอิง *sync.WaitGroup
 	go func() {
-		fmt.Println("Hello, world : A")
+		time.Sleep(1 * time.Second)
+		fmt.Println("B")
 		wg.Done() // บอก WaitGroup ว่า goroutine นี้เสร็จสิ้นแล้ว
 	}()
-
-	go SayB(&wg) // ส่งทางการอ้างอิง *sync.WaitGroup
-
 	go func() {
-		fmt.Println("Hello, world : C")
+		time.Sleep(1 * time.Second)
+		fmt.Println("C")
 		wg.Done()
 	}()
-
-	go func() {
-		fmt.Println("Hello, world : D")
-		wg.Done()
-	}()
-
 	wg.Wait() // รอให้ทุก goroutine ใน WaitGroup เสร็จสิ้น
-	fmt.Println("All go routines have finished")
+	//fmt.Println("All go routines have finished")
+	fmt.Println("Total Run Time : ", time.Since(startAsyncTime))
 }
